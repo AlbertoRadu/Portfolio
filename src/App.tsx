68fd8ba7e7441302d4ddda560cd8d1a72a5e6c7a
AlbertoRadu <gabrielalbertoradu@gmail.com>
2026-07-18 17:32:12 +0200

    feat: integrate TargetCursor and update background to black

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import Hero from './components/sections/Hero'
 import AboutMe from './components/sections/AboutMe'
+import Technologies from './components/sections/Technologies'
+import TargetCursor from './components/ui/TargetCursor'
 
 function App() {
   return (
-    <div className="bg-gray-950 text-white min-h-screen">
+    <div className="bg-black text-white min-h-screen">
+      <TargetCursor 
+        spinDuration={2}
+        hideDefaultCursor
+        parallaxOn
+        hoverDuration={0.2}
+        cursorColor="#ffffff"
+        cursorColorOnTarget="#7700e6"
+      />
       <Hero />
       <AboutMe />
+      <Technologies />
     </div>
   )
 }
